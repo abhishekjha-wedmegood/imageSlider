3fe1aa80aeb8232ec35636b514b5ea7c60089e12
Abhishek Jha <abhishek_jha@wedmegood.com>
2019-01-13 14:31:51 +0530

    Added a function to make the slider JSX dynamic

--- screens/LandingPage.js
+++ screens/LandingPage.js
@@ -1,43 +1,74 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import Slider from "react-slick";
 
-import getSearchResultsFromAPI from '../actions/searchImage';
+import getSearchResultsFromAPI from "../actions/searchImage";
 
 class LandingPage extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        refreshing: false,
-        data: [],
-        query: '',
-        page: 1
-    }
+      refreshing: false,
+      data: [],
+      query: "",
+      page: 1
+    };
     // this.props.getSearchResultsFromAPI(this.state.query, this.state.page);
-    this.props.getSearchResultsFromAPI('coffee', 1);
-  };
-  
-  render() {
+    this.props.getSearchResultsFromAPI("coffee", 1);
+  }
+
+  imageSlide = url => {
     return (
       <div>
-          test
+        <img src={url} />
       </div>
+    );
+  };
+
+  slideLoader = () => {
+    var slides = [];
+    this.props.urlList.forEach(element => {
+      slides.push(this.imageSlide(element));
+    })
+    return slides;
+  }
+
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      autoplaySpeed: 700,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      centerMode: true
+    };
+    return (
+      <Slider {...settings}>
+        {this.slideLoader()}
+      </Slider>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return{
+const mapStateToProps = state => {
+  return {
     urlList: state.searchImage.urlList
   };
- }
- 
- const matchDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    getSearchResultsFromAPI: getSearchResultsFromAPI
-  }, dispatch);
- }
+};
 
-export default connect(mapStateToProps,matchDispatchToProps)(LandingPage);
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      getSearchResultsFromAPI: getSearchResultsFromAPI
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(LandingPage);
